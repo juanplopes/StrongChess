2aff029232ad16065b1df88f1ba6f7ab96fe4c97
ElemarJR <elemar@procad.net>
2011-05-17 11:02:54 -0300

    Extracting MakePawnMove method

--- src/StrongChess.Model/Board.cs
+++ src/StrongChess.Model/Board.cs
@@ -59,102 +59,21 @@ namespace StrongChess.Model
             var moving = (this.IsWhiteTurn ? White : Black);
             var notmoving = (this.IsWhiteTurn ? Black : White);
 
-            if (moving.GetPieceAt(move.From) == ChessPieces.Pawn)
+            switch (moving.GetPieceAt(move.From))
             {
-                bool isPromotion = 
-                    (move.To.Rank.Index == 7 || move.To.Rank.Index == 0);
-
-                if (move.Type == MoveTypes.Normal && isPromotion)
-                    throw new InvalidMoveException(move, this);
-                
-                var isvalid = moving.Pawns
-                    .GetAllMoves(~Occupation, notmoving.Occupation, 
-                    this.Enpassant)
-                    .Count(m => m.From == move.From && m.To == move.To)
-                    > 0;
-
-                if (!isvalid)
-                    throw new InvalidMoveException(move, this);
-
-                var locations = moving.Pawns.Locations
-                    & (~move.From.AsBoard);
-
-                if (!isPromotion)
-                    locations |= move.To.AsBoard;
-
-                IPawns pawns = IsWhiteTurn ? 
-                    (IPawns) new WhitePawns(locations) :
-                    (IPawns) new BlackPawns(locations);
-
-                if (move.From.File == move.To.File && 
-                    Math.Abs(move.From.Rank - move.To.Rank) > 1)
-                    enpassant = new Square(
-                        (move.From.Rank + move.To.Rank) / 2,
-                        move.From.File
-                        );
-
-                var queens = moving.Queens;
-                if (move.Type == MoveTypes.PawnToQueenPromotion)
-                    queens = new PieceSet<Queen>(
-                        moving.Queens.Locations |
-                        move.To.AsBoard
-                        );
-
-                var bishops = moving.Bishops;
-                if (move.Type == MoveTypes.PawnToBishopPromotion)
-                    bishops = new PieceSet<Bishop>(
-                        moving.Bishops.Locations |
-                        move.To.AsBoard
-                        );
-
-                var knights = moving.Knights;
-                if (move.Type == MoveTypes.PawnToKnightPromotion)
-                    knights = new PieceSet<Knight>(
-                        moving.Knights.Locations |
-                        move.To.AsBoard
-                        );
-
-                var rooks = moving.Rooks;
-                if (move.Type == MoveTypes.PawnToRookPromotion)
-                    rooks = new PieceSet<Rook>(
-                        moving.Rooks.Locations |
-                        move.To.AsBoard
-                        );
-
-                moving = new Side(
-                    moving.KingLocation,
-                    queens,
-                    bishops,
-                    knights,
-                    rooks,
-                    pawns
-                    );
-
-                Bitboard negative;
-                if (move.To != Enpassant)
-                    negative = ~move.To.AsBoard;
-                else if (move.To.Rank == 6)
-                    negative = ~(new Square(5, move.To.File).AsBoard);
-                else 
-                    negative = ~(new Square(4, move.To.File).AsBoard);
-
-                notmoving = new Side(
-                    notmoving.KingLocation,
-                    new PieceSet<Queen>(notmoving.Queens.Locations & negative),
-                    new PieceSet<Bishop>(notmoving.Bishops.Locations & negative),
-                    new PieceSet<Knight>(notmoving.Knights.Locations & negative),
-                    new PieceSet<Rook>(notmoving.Rooks.Locations & negative),
-                    (IsWhiteTurn ?
-                        (IPawns)new BlackPawns(notmoving.Pawns.Locations & negative)
-                        :
-                        (IPawns)new WhitePawns(notmoving.Pawns.Locations & negative)
-                      )
-                    );
-            }
-            else
-            {
-                throw new NotImplementedException(
-                    string.Format("There is no support to {0} moves", moving.GetPieceAt(move.From))
+                case ChessPieces.Pawn:
+                    MakePawnMove(move, ref enpassant, ref moving, ref notmoving);
+                    break;
+                case ChessPieces.King:
+                case ChessPieces.Queen:
+                case ChessPieces.Bishop:
+                case ChessPieces.Knight:
+                case ChessPieces.Rook:
+                case ChessPieces.None:
+                default:
+                    throw new NotImplementedException(
+                        string.Format("There is no support to {0} moves", 
+                        moving.GetPieceAt(move.From))
                     );
             }
 
@@ -162,6 +81,102 @@ namespace StrongChess.Model
             var black = (this.IsWhiteTurn ? notmoving : moving);
             
             return new Board(white, black, 0, !IsWhiteTurn, enpassant);
+        }
+
+        private void MakePawnMove(Move move, 
+            ref Square? enpassant, 
+            ref Side moving, 
+            ref Side notmoving)
+        {
+            bool isPromotion =
+                (move.To.Rank.Index == 7 || move.To.Rank.Index == 0);
+
+            if (move.Type == MoveTypes.Normal && isPromotion)
+                throw new InvalidMoveException(move, this);
+
+            var isvalid = moving.Pawns
+                .GetAllMoves(~Occupation, notmoving.Occupation,
+                this.Enpassant)
+                .Count(m => m.From == move.From && m.To == move.To)
+                > 0;
+
+            if (!isvalid)
+                throw new InvalidMoveException(move, this);
+
+            var locations = moving.Pawns.Locations
+                & (~move.From.AsBoard);
+
+            if (!isPromotion)
+                locations |= move.To.AsBoard;
+
+            IPawns pawns = IsWhiteTurn ?
+                (IPawns)new WhitePawns(locations) :
+                (IPawns)new BlackPawns(locations);
+
+            if (move.From.File == move.To.File &&
+                Math.Abs(move.From.Rank - move.To.Rank) > 1)
+                enpassant = new Square(
+                    (move.From.Rank + move.To.Rank) / 2,
+                    move.From.File
+                    );
+
+            var queens = moving.Queens;
+            if (move.Type == MoveTypes.PawnToQueenPromotion)
+                queens = new PieceSet<Queen>(
+                    moving.Queens.Locations |
+                    move.To.AsBoard
+                    );
+
+            var bishops = moving.Bishops;
+            if (move.Type == MoveTypes.PawnToBishopPromotion)
+                bishops = new PieceSet<Bishop>(
+                    moving.Bishops.Locations |
+                    move.To.AsBoard
+                    );
+
+            var knights = moving.Knights;
+            if (move.Type == MoveTypes.PawnToKnightPromotion)
+                knights = new PieceSet<Knight>(
+                    moving.Knights.Locations |
+                    move.To.AsBoard
+                    );
+
+            var rooks = moving.Rooks;
+            if (move.Type == MoveTypes.PawnToRookPromotion)
+                rooks = new PieceSet<Rook>(
+                    moving.Rooks.Locations |
+                    move.To.AsBoard
+                    );
+
+            moving = new Side(
+                moving.KingLocation,
+                queens,
+                bishops,
+                knights,
+                rooks,
+                pawns
+                );
+
+            Bitboard negative;
+            if (move.To != Enpassant)
+                negative = ~move.To.AsBoard;
+            else if (move.To.Rank == 6)
+                negative = ~(new Square(5, move.To.File).AsBoard);
+            else
+                negative = ~(new Square(4, move.To.File).AsBoard);
+
+            notmoving = new Side(
+                notmoving.KingLocation,
+                new PieceSet<Queen>(notmoving.Queens.Locations & negative),
+                new PieceSet<Bishop>(notmoving.Bishops.Locations & negative),
+                new PieceSet<Knight>(notmoving.Knights.Locations & negative),
+                new PieceSet<Rook>(notmoving.Rooks.Locations & negative),
+                (IsWhiteTurn ?
+                    (IPawns)new BlackPawns(notmoving.Pawns.Locations & negative)
+                    :
+                    (IPawns)new WhitePawns(notmoving.Pawns.Locations & negative)
+                  )
+                );
         }
         
     }
